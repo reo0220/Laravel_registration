a753c7aaec86f85e69ba7cc09dabdd68fde75804
reo0220 <k_reo0220@icloud.com>
2024-06-27 22:20:48 +0900

    アカウント登録画面実装1

---
+++ laravel_reg/resources/views/regist.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel = "stylesheet" type = "text/css" href = "{{ asset('resources/css/style.css') }}">
+        <title>アカウント登録画面</title>
+    </head>
+    <body>
+        <ul class = "a">
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>D.I.Blogについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+        </ul>
+    
+    </body>
+</html>
